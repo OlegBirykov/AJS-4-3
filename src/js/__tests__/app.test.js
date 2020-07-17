6315ec05f1b38cc14df8e6a9072ed7bf2945cd5c
OlegBirykov <p_petro75@mail.ru>
2020-07-17 20:50:13 +0300

    Инициализация проекта

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,3 @@
+import getLevel from '../app';
+
+getLevel();
---
+++ src/js/http.js
@@ -0,0 +1,6 @@
+export default function fetchData(url) {
+  if (!url) { // заглушка для линтера
+    return;
+  }
+  throw new Error('Mock this!');
+}
